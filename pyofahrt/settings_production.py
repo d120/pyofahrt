2a6144e743bada62b44fa085a744231f2f11ec5a
exploide <me@exploide.net>
2016-04-25 13:56:17 +0200

    restructured settings for production
    - settings_production imports all default settings and overrides respective ones
    - settings_production imports secrets from settings_secrets which is not set under version control

---
+++ pyofahrt/settings_production.py
@@ -0,0 +1,32 @@
+"""
+This is the settings file used in production.
+First, it imports all default settings, then overrides respective ones.
+Secrets are stored in and imported from an additional file, not set under version control.
+"""
+
+from pyofahrt.settings import *
+import pyofahrt.settings_secrets as secrets
+
+SECRET_KEY = secrets.SECRET_KEY
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['.fachschaft.informatik.tu-darmstadt.de', '.d120.de']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'NAME': 'pyofahrt',
+        'USER': 'pyofahrt',
+        'PASSWORD': secrets.DB_PASSWORD
+    }
+}
+
+STATIC_URL = '/pyofahrt/static/'
+
+ADMINS = (('FSS', 'fss@fachschaft.informatik.tu-darmstadt.de'),)
+
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
