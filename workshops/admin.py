97a72a6bac2525386f3510022808117661d79dfe
Chris <chris.wars@yahoo.de>
2016-04-29 13:24:43 +0200

    Workshops eingefügt

--- workshops/admin.py
+++ workshops/admin.py
@@ -1,8 +1,44 @@
 from django.contrib import admin
-from workshops.models import Workshop
+from workshops.models import Workshop, Slot
+
+germandays = {
+"0" : "Montag",
+"1" : "Dienstag",
+"2" : "Mittwoch",
+"3" : "Donnerstag",
+"4" : "Freitag",
+"5" : "Samstag",
+"6" : "Sonntag",
+}
+
 
 # Register your models here.
 
 @admin.register(Workshop)
 class WorkshopAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["name", "show_hosts", "show_slot"]
+    list_filter = ["host", "slot"]
+
+    def show_hosts(self, obj):
+        return ", ".join([str(t) for t in obj.host.all()])
+    show_hosts.short_description = "Anbieter"
+
+    def longstr(self, slot):
+        return slot.name + " - " + germandays[slot.begin.strftime('%w')] + " (" + slot.begin.strftime('%H:%M') + " - " + slot.end.strftime('%H:%M') + ")"
+
+    def show_slot(self, obj):
+        slotname = str(obj.slot)
+        slot = Slot.objects.get(name=slotname)
+        return self.longstr(slot)
+    show_slot.short_description = "Zeitslot"
+
+
+
+@admin.register(Slot)
+class SlotAdmin(admin.ModelAdmin):
+    list_display = ["name", "fromto", "slottype", "priority"]
+    date_hierarchy = "begin"
+
+    def fromto(self, slot):
+        return slot.begin.strftime('%H:%M') + " - " + slot.end.strftime('%H:%M') + " (" + germandays[slot.begin.strftime('%w')] + ")"
+    fromto.short_description = "Zeitrahmen"
--- workshops/models.py
+++ workshops/models.py
@@ -1,8 +1,57 @@
 from django.db import models
 from staff.models import WorkshopCandidate
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from ofahrtbase.models import Ofahrt
 
 # Create your models here.
 
+
+class Slot(models.Model):
+    class Meta:
+        verbose_name = "Zeitslot"
+        verbose_name_plural = "Zeitslots"
+
+
+    name = models.CharField("Bezeichnung", max_length=30, unique=True)
+    begin = models.DateTimeField("Start")
+    end = models.DateTimeField("Ende")
+
+    SLOT_CHOICES = (
+        ("workshop", "Workshop"),
+        ("event", "Zentrale Veranstaltung"),
+        ("food", "Essen"),
+        ("sonstiges", "Sonstiges")
+    )
+
+    PRIORITY_CHOICES = (
+        ("opt", "optional"),
+        ("zentral", "zentral"),
+        ("dezentral", "dezentral"),
+        ("wichtig", "wichtig")
+    )
+
+    slottype = models.CharField("Art des Slots", choices=SLOT_CHOICES, max_length=30, default="sonstiges")
+    priority = models.CharField("Priorität", choices=PRIORITY_CHOICES, max_length=30, default="zentral")
+
+
+    def __str__(self):
+        return self.name
+
+    def clean(self):
+        if self.begin > self.end:
+            raise ValidationError("Der Startzeitpunkt muss vor dem Entzeitpunkt liegen")
+
+
 class Workshop(models.Model):
+    class Meta:
+        verbose_name = "Workshop"
+        verbose_name_plural = "Workshops"
+
+    base = models.ForeignKey(Ofahrt, verbose_name="Zugehörige Ofahrt")
+
     name = models.CharField("Titel", max_length=50)
     description = models.TextField("Beschreibung", blank=True)
+    host = models.ManyToManyField(User, verbose_name="Workshopanbieter")
+
+    slot = models.ForeignKey(Slot, verbose_name="Zugeteilter Zeitslot", null=True, blank=True)
