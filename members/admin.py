616524a5062bccb6d5ebf0ebb6fbafedca4b7de6
Chris <chris.wars@yahoo.de>
2016-10-15 14:55:41 +0200

    add mail export for members close #41

--- members/admin.py
+++ members/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from .models import Member, FoodHandicaps
+from . import admin_actions
 
 # Register your models here.
 
 admin.site.register(FoodHandicaps)
 
+
 @admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
+    actions = [admin_actions.mail_export]
     list_display = ('first_name', 'last_name', 'gender', 'money_received', 'is_really_ersti', 'is_full_aged')
     list_filter = ('gender', 'money_received', 'is_really_ersti', 'room')
     fieldsets = (
