15e41cf17f32d09f56b018dd8650985eaa9d1780
Chris <chris.wars@yahoo.de>
2016-10-20 16:37:59 +0200

    fix age check for membersignups

--- members/forms.py
+++ members/forms.py
@@ -11,6 +11,6 @@ class SignupForm(forms.ModelForm):
 
     def clean_birth_date(self):
         begin_date = Ofahrt.current().begin_date
-        if begin_date.replace(year=begin_date.year - 18) > self.cleaned_data["birth_date"]:
+        if begin_date.replace(year=begin_date.year - 18) < self.cleaned_data["birth_date"]:
             raise forms.ValidationError("Eine Anmeldung ist nur für Teilnehmer*innen möglich, welche zum Zeitpunkt der Ofahrt volljährig sind.")
         return self.cleaned_data["birth_date"]
